b93cb1de32ab76dcddd36fafba8801145cc702ae
Maxim Sakovich <choochooh55@gmail.com>
2026-09-17 20:59:04 +0300

    rhythm analysis command

--- Sources/MusicUnderstandingCLI/main.swift
+++ Sources/MusicUnderstandingCLI/main.swift
@@ -2,11 +2,27 @@ import Foundation
 import MusicUnderstanding
 import AVFoundation
 
+
+let helpStr = "Usage: \n full analysis - mu-cli filepath \n only loudness - mu-cli --loudness filepath\n only rhythm —   mu-cli --rhythm filepath\n"
+
+
 let arguments = CommandLine.arguments
+
 guard arguments.count > 1 else {
-    FileHandle.standardError.write("Usage: \n full analysis - mu-cli /path/to/audio/file.mp3 \n only loudness - mu-cli -l /path/to/audio/file.mp3\n ".data(using: .utf8)!)
+    FileHandle.standardError.write(helpStr.data(using: .utf8)!)
     exit(1)
 }
+
+if arguments[1] == "--help" {
+    FileHandle.standardOutput.write(helpStr.data(using: .utf8)!)
+    exit(0)
+}
+
+if arguments[1] == "--version" || arguments[1] == "-v" {
+    FileHandle.standardOutput.write("Music Understanding CLI 0.3\n".data(using: .utf8)!)
+    exit(0)
+}
+
 
 let fileURL = URL(fileURLWithPath: arguments.last ?? "")
 
@@ -18,9 +34,12 @@ do {
 
     let session = try await MusicUnderstandingSession(asset: asset)
     
+
+    
     let results = switch arguments[1]  {
-        case "-l":  ( try await session.analyze(for: [.loudness]) )
-        default:    ( try await session.analyze() )}
+        case "--loudness", "-l":  ( try await session.analyze(for: [.loudness]) )
+        case "--rhythm", "-r":    ( try await session.analyze(for: [.rhythm]) )
+        default:                  ( try await session.analyze() )}
 
     let encoder = JSONEncoder()
     encoder.outputFormatting = [.prettyPrinted, .sortedKeys]
